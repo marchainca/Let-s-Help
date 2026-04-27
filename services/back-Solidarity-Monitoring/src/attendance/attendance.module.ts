0bc34851b2d9cb76ab88959fc7ab02af1a4dba8a
John Restrepo <marchainca@gmail.com>
2026-04-27 10:54:05 -0500

    create data base service in attendance

--- services/back-Solidarity-Monitoring/src/attendance/attendance.module.ts
+++ services/back-Solidarity-Monitoring/src/attendance/attendance.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { AttendanceController } from './attendance.controller';
+import { DataBaseService } from './data-base.service';
 
 @Module({
-  providers: [AttendanceService],
+  providers: [AttendanceService, DataBaseService],
   controllers: [AttendanceController]
 })
 export class AttendanceModule {}

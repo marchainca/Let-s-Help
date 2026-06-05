1fbc28e6420be4b895dceb055a92023da81c1e86
John Restrepo <marchainca@gmail.com>
2026-06-05 16:29:19 -0500

    update dashboard module

--- services/back-Solidarity-Monitoring/src/dashboard/dashboard.module.ts
+++ services/back-Solidarity-Monitoring/src/dashboard/dashboard.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
+import { DataBaseDashboardService } from './data-base-dashboard.service';
+import { ActivityTracking } from 'src/activities/entities/activity-tracking.entity';
+import { Attendance } from 'src/attendance/entities/attendance.entity';
+import { User } from 'src/users/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [DashboardService],
+  imports: [
+    TypeOrmModule.forFeature([ActivityTracking, Attendance, User]),
+  ],
+  providers: [DashboardService, DataBaseDashboardService],
   controllers: [DashboardController]
 })
 export class DashboardModule {}

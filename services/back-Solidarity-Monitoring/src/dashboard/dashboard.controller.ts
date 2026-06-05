e8baf909dc5cc98e74a9ff0a302ea5fc1c89772e
John Restrepo <marchainca@gmail.com>
2026-06-05 16:28:45 -0500

    update dashboard controller

--- services/back-Solidarity-Monitoring/src/dashboard/dashboard.controller.ts
+++ services/back-Solidarity-Monitoring/src/dashboard/dashboard.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { DashboardService } from './dashboard.service';
+import { sendResponse } from 'src/tools/function.tools';
+import params from 'src/tools/params';
 
-@Controller('dashboard')
-export class DashboardController {}
+@Controller('letsHelp/Colombia/dashboard')
+export class DashboardController {
+
+    constructor(
+        private readonly dashboardService: DashboardService,
+    ) {}
+
+    @Get('/indicators')
+    async getIndicators() {
+        try {
+            const getIndicators = await this.dashboardService.getDashboardIndicators();
+            return await sendResponse(true, params.ResponseMessages.MESSAGE_SUCCESS, getIndicators );
+        } catch (error) {
+            throw new HttpException(
+            {
+                code: error.code,
+                message: error.message,
+                attribute: error.attribute,
+                statusCode: error.statusCode,
+            },
+                HttpStatus.BAD_REQUEST,
+            );
+        }
+    }
+}

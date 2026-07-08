d8376090ca02656ae30fe1007523147f5f106e22
John Restrepo <marchainca@gmail.com>
2026-07-08 16:04:38 -0500

    uptdate paths

--- apps/web-dashboard/lets-help-dashboard/src/params.ts
+++ apps/web-dashboard/lets-help-dashboard/src/params.ts
@@ -1,8 +1,15 @@
+import { create } from "domain";
+
 export default{
     paths:{
         activities: '/letsHelp/Colombia/activities',
-        activitiesBySubprogram: '/letsHelp/Colombia/activities-by-subprogram',
+        activitiesBySubprogram: '/letsHelp/Colombia/activities/activities-by-subprogram',
         getPrograms: '/letsHelp/Colombia/activities/getPrograms',
         getSubprograms: '/letsHelp/Colombia/activities/getSubprograms',
+        createProgram: '/letsHelp/Colombia/activities/createProgram',
+        createSubprogram: '/letsHelp/Colombia/activities/createSubprogram',
+        createActivity: '/letsHelp/Colombia/activities/createActivity',
+        updateActivity: '/letsHelp/Colombia/activities/update-activity',
+        deleteActivity: '/letsHelp/Colombia/activities/deleteActivity',
     }
 }
